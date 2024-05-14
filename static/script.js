9f8e80fb3d2dadd996bb628b0e096026359c6430
Thomas Kilian <kiliant@mailbox.org>
2024-05-14 13:23:43 +0200

    ui improvements

--- static/script.js
+++ static/script.js
@@ -15,6 +15,14 @@ document.getElementById('anonymizeForm').addEventListener('submit', function(e) 
         .replace(/>/g, "&gt;");
         console.log(formattedData);
         document.getElementById('result').innerHTML = `Anonymisierter Text:<br />${formattedData}`;
+        document.getElementById('result').style.display = 'block';
+        document.getElementById('deleteButton').style.display = 'inline-block';
     })
     .catch(error => console.error('Error:', error));
+});
+
+document.getElementById('deleteButton').addEventListener('click', function() {
+    document.getElementById('result').innerHTML = '';
+    document.getElementById('result').style.display = 'none';
+    document.getElementById('deleteButton').style.display = 'none';
 });
